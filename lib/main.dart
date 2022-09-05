e5357895548fdbc6eb63a3285b1502c31c996625
Happi3r <hsphs0101@gmail.com>
2022-09-06 00:15:20 +0900

    Revert "?"
    This reverts commit f6c0940e5f593c3167d1157745f3c343d382b4d2.

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,231 @@
 import 'package:flutter/material.dart';
+import 'package:miniplayer/miniplayer.dart';
 import 'package:provider/provider.dart';
 import 'package:shaval/player.dart';
-import 'package:youtube_explode_dart/youtube_explode_dart.dart';
+import 'package:youtube_player_iframe/youtube_player_iframe.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => ViewModel()),
-      ],
-      child: const Main(),
-    ),
-  );
+  runApp(const Youtube());
 }
 
-class Main extends StatefulWidget {
-  const Main({Key? key}) : super(key: key);
+class Youtube extends StatefulWidget {
+  const Youtube({Key? key}) : super(key: key);
 
   @override
-  State<Main> createState() => _MainState();
+  State<Youtube> createState() => _YoutubeState();
 }
 
-class _MainState extends State<Main> {
-  ViewModel? viewModel;
+class _YoutubeState extends State<Youtube> {
+  final _controller = YoutubePlayerController();
+  final List<Song> ids = const [
+    Song(
+      id: 'DPEtmqvaKqY',
+      artist: '고세구',
+      image: '',
+      title: '팬서비스',
+    ),
+    Song(
+      id: 'JY-gJkMuJ94',
+      artist: '이세계 아이돌',
+      image: '',
+      title: '겨울봄',
+    ),
+    Song(
+      id: '6hEvgKL0ClA',
+      artist: '릴파',
+      image: '',
+      title: 'Promise',
+    ),
+    Song(
+      id: '08meo6qrhFc',
+      artist: '우왁굳',
+      image: '',
+      title: '왁맥송',
+    ),
+    Song(
+      id: 'rFxJjpSeXHI',
+      artist: '주르르',
+      image: '',
+      title: 'SCIENTIST',
+    ),
+    Song(
+      id: 'K-5WdjbCYnk',
+      artist: '릴파, 뢴트게늄',
+      image: '',
+      title: '마지막 재회',
+    ),
+    Song(
+      id: 'Empfi8q0aas',
+      artist: '이세계 아이돌',
+      image: '',
+      title: '이세돌 싸이퍼',
+    ),
+    Song(
+      id: '1UbyyaDc8x0',
+      artist: '릴파',
+      image: '',
+      title: '세사빠',
+    ),
+    Song(
+      id: 'fgSXAKsq-Vo',
+      artist: '이세계 아이돌',
+      image: '',
+      title: '리와인드',
+    ),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _controller.playVideo();
+  }
+
+  void wtf() {
+    print(_controller.metadata.videoId); //i1
+    if (Playlist().now?.id != _controller.metadata.videoId) {
+      //i0
+      Playlist().now == null
+          ? _controller.stopVideo() //n1
+          : _controller.loadVideoById(videoId: Playlist().now!.id); //n0
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    viewModel = Provider.of<ViewModel>(context);
-    return MaterialApp(
-      home: Scaffold(
-        body: SafeArea(
-          child: Column(
-            children: [],
+    return ChangeNotifierProvider(
+      create: (context) => Playlist()..addListener(wtf),
+      child: MaterialApp(
+        home: Consumer<Playlist>(
+          builder: (context, playlist, _) => YoutubePlayerScaffold(
+            controller: _controller,
+            builder: (context, player) {
+              return Scaffold(
+                body: SafeArea(
+                  child: Stack(
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(playlist.now?.title ?? '없음'),
+                          Text(_controller.metadata.title),
+                          SizedBox(
+                            width: 320,
+                            height: 180,
+                            child: player,
+                          ),
+                          Wrap(
+                            children: ids
+                                .map(
+                                  (e) => TextButton(
+                                    onPressed: () => playlist.add(e),
+                                    child: Text(e.title),
+                                  ),
+                                )
+                                .toList(),
+                          ),
+                          YoutubeValueBuilder(
+                            builder: (context, value) => Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                IconButton(
+                                  onPressed: () {},
+                                  icon: const Icon(Icons.skip_previous),
+                                ),
+                                IconButton(
+                                  onPressed: playlist.prev,
+                                  icon: const Icon(Icons.skip_previous),
+                                ),
+                                IconButton(
+                                  onPressed:
+                                      value.playerState == PlayerState.playing
+                                          ? _controller.pauseVideo
+                                          : _controller.playVideo,
+                                  icon: Icon(
+                                      value.playerState == PlayerState.playing
+                                          ? Icons.pause
+                                          : Icons.play_arrow),
+                                ),
+                                IconButton(
+                                  onPressed: playlist.next,
+                                  icon: const Icon(Icons.skip_next),
+                                ),
+                                IconButton(
+                                  onPressed: () {
+                                    print(value.playerState);
+                                    _controller.stopVideo();
+                                    playlist.clear();
+                                  },
+                                  icon: const Icon(Icons.close),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      const Player(),
+                    ],
+                  ),
+                ),
+              );
+            },
           ),
         ),
-        bottomNavigationBar: const Player(),
       ),
     );
   }
+
+  @override
+  void dispose() {
+    _controller.close();
+    Playlist().removeListener(wtf);
+    super.dispose();
+  }
 }
 
-class ViewModel extends ChangeNotifier {}
+class Song {
+  final String id;
+  final String title;
+  final String artist;
+  final String image;
+
+  const Song({
+    required this.id,
+    required this.title,
+    required this.artist,
+    required this.image,
+  });
+}
+
+class Playlist extends ChangeNotifier {
+  static Playlist? _instance;
+  Playlist._();
+  factory Playlist() => _instance ??= Playlist._();
+
+  List<Song> list = List.empty(growable: true);
+
+  Song? get now => list.isEmpty ? null : list[_index];
+
+  int _index = 0;
+  int get index => _index;
+
+  void add(Song s) {
+    list.add(s);
+    notifyListeners();
+  }
+
+  void prev() {
+    _index = _index == 0 ? list.length - 1 : _index - 1;
+    notifyListeners();
+  }
+
+  void next() {
+    _index = _index == list.length - 1 ? 0 : _index + 1;
+    notifyListeners();
+  }
+
+  void clear() {
+    _index = 0;
+    list.clear();
+    notifyListeners();
+  }
+}
--- lib/player.dart
+++ lib/player.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:youtube_explode_dart/youtube_explode_dart.dart';
+import 'package:miniplayer/miniplayer.dart';
+import 'package:provider/provider.dart';
+import 'package:shaval/main.dart';
+import 'package:youtube_player_iframe/youtube_player_iframe.dart';
 
 class Player extends StatefulWidget {
   const Player({Key? key}) : super(key: key);
@@ -11,6 +14,152 @@ class Player extends StatefulWidget {
 class _PlayerState extends State<Player> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final ytCtrl = context.ytController;
+    Playlist playlist = Provider.of<Playlist>(context);
+    return Miniplayer(
+      minHeight: 62,
+      maxHeight: 240,
+      builder: (height, percent) => playlist.now != null
+          ? Column(
+              children: [
+                Container(
+                  width: double.infinity,
+                  height: 60,
+                  color: Colors.white,
+                  padding: const EdgeInsets.all(5),
+                  child: Row(
+                    children: [
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      const Icon(
+                        Icons.queue_music,
+                        size: 28,
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Expanded(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(playlist.now!.title,
+                                overflow: TextOverflow.fade,
+                                softWrap: false,
+                                style: const TextStyle(
+                                  fontSize: 14,
+                                  fontFamily: "Pretendard",
+                                  fontWeight: FontWeight.w700,
+                                )),
+                            Text(playlist.now!.artist,
+                                overflow: TextOverflow.fade,
+                                softWrap: false,
+                                style: const TextStyle(
+                                  fontSize: 12,
+                                  fontFamily: "Pretendard",
+                                  fontWeight: FontWeight.w600,
+                                )),
+                          ],
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: playlist.prev,
+                        icon: const Icon(
+                          Icons.skip_previous,
+                          size: 30,
+                        ),
+                      ),
+                      YoutubeValueBuilder(
+                        builder: (context, value) => IconButton(
+                          onPressed: value.playerState == PlayerState.playing
+                              ? ytCtrl.pauseVideo
+                              : ytCtrl.playVideo,
+                          icon: Icon(
+                            value.playerState == PlayerState.playing
+                                ? Icons.pause
+                                : Icons.play_arrow,
+                            size: 30,
+                          ),
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: playlist.next,
+                        icon: const Icon(
+                          Icons.skip_next,
+                          size: 30,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const DurationProgressIndicator(
+                  type: ProgressType.mini,
+                ),
+              ],
+            )
+          : Container(),
+    );
+  }
+}
+
+enum ProgressType { mini, slider }
+
+class DurationProgressIndicator extends StatefulWidget {
+  const DurationProgressIndicator({Key? key, required this.type})
+      : super(key: key);
+  final ProgressType type;
+
+  @override
+  State<DurationProgressIndicator> createState() =>
+      _DurationProgressIndicatorState();
+}
+
+class _DurationProgressIndicatorState extends State<DurationProgressIndicator> {
+  @override
+  Widget build(BuildContext context) {
+    final ytCtrl = context.ytController;
+    return Consumer<Playlist>(
+      builder: (context, playlist, _) => StreamBuilder<Duration>(
+        stream: ytCtrl.getCurrentPositionStream(),
+        initialData: Duration.zero,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            print('sibalalaaal ${snapshot.error}');
+          }
+          num position = snapshot.data?.inMilliseconds ?? 0;
+          final duration = ytCtrl.metadata.duration.inMilliseconds;
+          switch (widget.type) {
+            case ProgressType.mini:
+              if (position != 0 && position == duration) {
+                playlist.next();
+              }
+              return LinearProgressIndicator(
+                value: duration == 0 ? 0 : position / duration,
+                minHeight: 2,
+                backgroundColor: Colors.transparent,
+              );
+            case ProgressType.slider:
+              if (position != 0 && position == duration) {
+                playlist.next();
+              }
+              double value = position.toDouble();
+              print('$value ||| $position ||| $duration');
+              return Slider(
+                max: duration.toDouble(),
+                value: value,
+                onChanged: (v) => setState(() {
+                  print('$v <<<<<');
+                  value = v;
+                }),
+                onChangeEnd: (v) => ytCtrl.seekTo(
+                  seconds: v / 1000,
+                  allowSeekAhead: true,
+                ),
+              );
+          }
+        },
+      ),
+    );
   }
 }
